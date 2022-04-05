d163478e5e0b335a2f411534ab8fc580540a4665
Tiago Moraes <tiagoefmoraes@gmail.com>
2022-04-05 20:32:08 -0300

    CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExUp.MixProject do
     [
       app: :ex_up,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
